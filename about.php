ee25067ac44a9d118bcd4372cc0bda75b6e890b0
vadim-51 <ideaintech.vadim@gmail.com>
2017-04-20 12:55:37 +0300

    about

---
+++ about.php
@@ -0,0 +1,31 @@
+<title>About</title>
+<div class="pageContent">
+<br/>
+<p class="page">
+Website TCI was created using programming languages PHP and JavaScript. Being logged in as administrator, you will be given an additional tab “Admin”, which allows creating new content, and editing or deleting existing one. After creation all articles  are stored on the server database. This allows to quickly add any content and edit it, change images, etc. This part is accomplished using PHP. Admin page has an option of adding or deleting any number of paragraphs, headers, ordered or unordered lists. JavaScript code with jQuery allows reacting to user’s action of adding or deleting required number of HTML elements or changing images. This option is also included on the page for editing, so user can add any content he wants even after loading content on the server. JavaScript “puts” all this information into an array, which after that will be checked, analyzed and transferred to MySQL via PHP. The last part is implemented with PDO and prepared statements.
+</p>
+<p class="page">
+The website allows signing up, logging in, and logging out. I’ve created this part with Ajax. So the website is more responsive and allows changing some parts of it without any reloading (changing tabs “sign up” and “log in” on “log out” and vice versa, appearing or disappearing admin tab). Validation of forms is accomplished with jQuery validation plugin, which allows to quickly react on any user’s errors without reloading of the page, because it checks this part on the client's side. There is also some validation of forms on the server in the case a user turns off JavaScript.
+</p>
+<p class="page">
+The website has a feature to load more content when scrollbar reaches end of the page. This is possible with JavaScript and Ajax. No reloading of the page. On the server PHP code makes all necessary actions (with simple query to MySQL) to bring five additional articles. Part with smooth appearing of pages is accomplished using fadeIn() method of JQuery when element has display property of “none”, and hide() with fadeIn() when an element is visible.
+</p>
+<p class="page">
+Before creating even small snippet of code I stumbled with the most frequent questions for a beginner: where can I get all the information about web developing and how can I prevent wasting of time on reading inappropriate books or websites. Surfing the Internet with this idea I’ve got some understanding about this, which, I hope, could be useful for someone else. 
+</p>
+<p class="page">
+If you have not any idea about programming languages, you can visit the website <a href="https://www.w3schools.com" target="_blank">https://www.w3schools.com</a>. It contains all necessary information for first understanding. You can also have some practice, because the website has demonstration of examples and you can change code, add something new, etc. It has tutorials, references, quizzes. Besides necessary information about HTML, CSS, JavaScript, SQL and PHP, the website also contains tutorials for frameworks, such as Bootstrap and jQuery. 
+</p>
+<p class="page">
+Very helpful books about CSS for beginner is “CSS3: The Missing Manual”, O’Reilly. I think all series “Missing Manual” is very useful no matter what subject the book has. This is a respected publisher and you can get more understanding of subject by reading its books. This book also contains practical lessons, which you can download from official website. There are Missing Manual for JavaScript, jQuery and PHP. All of them worth reading.
+</p>
+<p class="page">
+There is a good book for learning PHP and MySQL, which has practical example. Actually , you can create website from scratch using this book. It has examples for form validation, creation shop card, web chat, etc. This book has title “PHP and MySQL Web Development” by Luke Welling and Laura Thomson. 
+</p>
+<p class="page">
+Do not forget to visit <a href="http://stackoverflow.com" target="_blank">http://stackoverflow.com</a>. This website has a huge community of programmers and a lot of code snippets. You can post your question, just use search before to make sure it’s not a duplicate. All helpful answers are voted up, which allows to not consider bad decisions for the question. 
+</p>
+<p class="page">
+Email: myatnik.vadi@gmail.com
+</p>
+</div>
